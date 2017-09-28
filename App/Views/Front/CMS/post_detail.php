5af2e1714d1b11519b59b53a0af21bd2c2fc8b1e
preetibendal <preeti.bendal@arsenaltech.com>
2017-09-28 09:17:00 +0530

    Integrated disqus api

--- App/Views/Front/CMS/post_detail.php
+++ App/Views/Front/CMS/post_detail.php
@@ -1,6 +1,8 @@
 <?php
 include(\App\Config::F_ROOT . 'App/Views/Front/header.php');
 $getFeaturedBanners = Core\Helper::getFeaturedBanners();
+$disqusPageUrl = \App\Config::W_ROOT.$post['slug'];
+$disqusPageIdentifier = $post['slug'];
 ?>
 <div id="fb-root"></div>
 <script>(function (d, s, id) {
@@ -93,26 +95,11 @@ $getFeaturedBanners = Core\Helper::getFeaturedBanners();
                             </div>
                         </div>
                     </div>
+                    <!-- Disqus Commenting Start -->
                     <div class="col-md-12">
-                        <div id="disqus_thread"></div>
-                        <script>
-
-                            /**
-                             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-                             var disqus_config = function () {
-                             this.page.url = '<?php echo \App\Config::W_ROOT.$post['slug']; ?>';  // Replace PAGE_URL with your page's canonical URL variable
-                             this.page.identifier = '<?php echo $post['slug'];?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                             };
-                            (function() { // DON'T EDIT BELOW THIS LINE
-                                var d = document, s = d.createElement('script');
-                                s.src = 'https://thewallreport.disqus.com/embed.js';
-                                s.setAttribute('data-timestamp', +new Date());
-                                (d.head || d.body).appendChild(s);
-                            })();
-                        </script>
-                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                        <?php include_once (\App\Config::F_VIEW.'disqus.php');?>
                     </div>
+                    <!-- Disqus Commenting End -->
                 </div>
 
             </div>
