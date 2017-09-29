f798ce0425d9c841a540b4dca367ff31626024d8
Dhaval Prajapati <dhaval.prajapati@arsenaltech.com>
2017-09-29 12:13:48 +0530

    Merge branch 'develop' of https://github.com/ast-developers/thewallreport into develop
    Conflicts:
    	Core/Helper.php

--- App/Config.php
+++ App/Config.php
@@ -2,23 +2,6 @@
 
 namespace App;
 
-    /**
-     * Class Config
-     * Application configuration
-     * @package App
-     */
-    /**
-     * Class Config
-     * @package App
-     */
-/**
- * Class Config
- * @package App
- */
-/**
- * Class Config
- * @package App
- */
 /**
  * Class Config
  * @package App
@@ -36,7 +19,7 @@ class Config
      * Database name
      * @var string
      */
-    const DB_NAME = 'wallreport';
+    const DB_NAME = 'thewallreport';
 
     /**
      * Database user
@@ -48,7 +31,57 @@ class Config
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'mysql';
+    const DB_PASSWORD = 'Ars3nal$0!';
+
+    /**
+     * SMTP HOST
+     */
+    const SMTP_HOST = 'smtp.gmail.com';
+
+    /**
+     * SMTP PORT
+     */
+    const SMTP_PORT = '587';
+
+    /**
+     * SMTP USERNAME
+     */
+    const SMTP_USERNAME = 'developers@arsenaltech.com';
+
+    /**
+     * SMTP PASSWORD
+     */
+    const SMTP_PASSWORD = 'Ar5ena1T3ch#0ev';
+
+    /**
+     * Website Root
+     */
+    const W_ROOT = "http://66.175.214.21/thewallreport/";
+
+    /**
+     * File Root
+     */
+    const F_ROOT = "/var/www/html/thewallreport/";
+
+    /**
+     * Contact us Email
+     */
+    const CONTACT_US_TO_EMAIL = "developers@arsenaltech.com";
+
+    /**
+     * Google Recaptcha site key
+     */
+    const GOOGLE_RECAPTCHA_SITE_KEY = "6LfE7zEUAAAAAFWx18Ig3tHozskPh0tzYxqfvJeF";
+
+    /**
+     * Google Recaptcha secret key
+     */
+    const GOOGLE_RECAPTCHA_SECRET_KEY = "6LfE7zEUAAAAAIA1In49BZ7EcUQ-WXtOq5nZDKz9";
+
+    /**
+     * Share on facebook App id
+     */
+    const SHARE_ON_FACEBOOK_APP_ID = "311382379200231";
 
     /**
      * Show or hide error messages on screen
@@ -60,33 +93,6 @@ class Config
      * App Page Title
      */
     const PAGE_TITLE_PREFIX = 'The Wall Report';
-
-    /**
-     * SMTP HOST
-     */
-    const SMTP_HOST = 'smtp.gmail.com';
-    /**
-     * SMTP PORT
-     */
-    const SMTP_PORT = '587';
-    /**
-     * SMTP USERNAME
-     */
-    const SMTP_USERNAME = 'dhaval.prajapati333@gmail.com';
-    /**
-     * SMTP PASSWORD
-     */
-    const SMTP_PASSWORD = 'Dhaval333';
-
-    /**
-     * Website Root
-     */
-    const W_ROOT = "http://local.thewall.report/";
-
-    /**
-     * File Root
-     */
-    const F_ROOT = "/home/devendra-bhati/public_html/thewallreport/";
 
     /**
      * File Assets Root
@@ -118,37 +124,22 @@ class Config
      */
     const W_FRONT_ASSETS = self::W_ROOT . "theme/front/assets/";
     /**
-     * Redactor Images Path
+     *
      */
     const F_REDACTOR_IMAGE = self::F_ROOT . "public/uploads/redactor_image/";
     /**
-     * Redactor Images Path
+     *
      */
     const W_REDACTOR_IMAGE = self::W_ROOT . "uploads/redactor_image/";
-    /**
-     * Contact us Email
-     */
-    const CONTACT_US_TO_EMAIL = "developers@arsenaltech.com";
-    /**
-     * Google Recaptcha site key
-     */
-    const GOOGLE_RECAPTCHA_SITE_KEY = "6LfE7zEUAAAAAFWx18Ig3tHozskPh0tzYxqfvJeF";
-    /**
-     * Google Recaptcha secret key
-     */
-    const GOOGLE_RECAPTCHA_SECRET_KEY = "6LfE7zEUAAAAAIA1In49BZ7EcUQ-WXtOq5nZDKz9";
+
     /**
      * Featured Image Path
      */
     const F_FEATURED_IMAGE_ROOT = self::F_ROOT . "public/uploads/featured_image/";
+
     /**
      * Featured Image Path
      */
     const W_FEATURED_IMAGE_ROOT = self::W_ROOT . "uploads/featured_image/";
-    /**
-     * Share on facebook App id
-     */
-    const SHARE_ON_FACEBOOK_APP_ID = "311382379200231";
-
 
 }
