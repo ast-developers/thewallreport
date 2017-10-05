558a2f3f0711c231deb06550c579967e8bfdae21
preetibendal <preeti.bendal@arsenaltech.com>
2017-10-05 12:02:14 +0530

    cleaned app.js and did code optimization for bulk delete functionality on listing page

--- public/theme/admin/assets/js/menu.js
+++ public/theme/admin/assets/js/menu.js
@@ -1,19 +1,19 @@
 var Menu = function () {
     var dataTable;
-    var initMenuDataTable = function () {
+    var initMenuDataTable   = function () {
         dataTable = $('#menu-grid').DataTable({
             "processing": true,
             "serverSide": true,
-            "order": [[1, "asc"]],
+            "order"     : [[1, "asc"]],
             "columnDefs": [{
-                "targets": 0,
-                "orderable": false,
+                "targets"   : 0,
+                "orderable" : false,
                 "searchable": false
 
             }],
-            "ajax": {
-                url: menuAjaxPaginateUrl, // json datasource
-                type: "post",  // method  , by default get
+            "ajax"      : {
+                url  : menuAjaxPaginateUrl, // json datasource
+                type : "post",  // method  , by default get
                 error: function (data) {  // error handling
                     $(".user-grid-error").html("");
                     $("#user-grid").append('<tbody class="user-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
@@ -23,69 +23,12 @@ var Menu = function () {
             }
         });
     };
-
-    var deleteMenu = function () {
-        $("#bulkDelete").on('click', function () { // bulk checked
-            var status = this.checked;
-            $(".deleteRow").each(function () {
-                $(this).prop("checked", status);
-            });
-            if ($("#bulkDelete").is(':checked')) {
-                $("#delete-btn").removeClass('hidden');
-            } else {
-                $("#delete-btn").addClass('hidden')
-            }
-        });
-        $(document).on('click', '.deleteRow', function (event) {
-            $("#delete-btn").addClass('hidden')
-            var $checked = false;
-
-            $.each($(".deleteRow"), function (index, value) {
-                if ($(this).is(':checked')) {
-                    $checked = true;
-                    $("#delete-btn").addClass('hidden')
-                }
-            });
-
-            if ($checked) {
-                $("#delete-btn").removeClass('hidden');
-            } else {
-                $('#bulkDelete').prop("checked", false);
-            }
-
-        });
-
-        $('#deleteMenus').on("click", function (event) { // triggering delete one by one
-            if ($('.deleteRow:checked').length > 0) {  // at-least one checkbox checked
-                var ids = [];
-                $('.deleteRow').each(function () {
-                    if ($(this).is(':checked')) {
-                        ids.push($(this).val());
-                    }
-                });
-                var ids_string = ids.toString();
-                // array to string conversion
-                $.ajax({
-                    type: "POST",
-                    url: menuBulkDeleteUrl,
-                    data: {data_ids: ids_string},
-                    success: function (result) {
-                        dataTable.draw();
-                        $('.header-title').after('<div class="alert alert-success">' +
-                            '<strong>' + 'Menus deleted successfully.' + '</strong></div>');
-                    },
-                    async: false
-                });
-            }
-        });
-    };
-
-    var validateMenu = function () {
+    var validateMenu        = function () {
         $('.menu-form').validate({
             errorElement: 'label', //default input error message container
-            errorClass: 'help-inline', // default input error message class
+            errorClass  : 'help-inline', // default input error message class
             focusInvalid: false, // do not focus the last invalid input
-            rules: {
+            rules       : {
                 name: {
                     required: true
                 },
@@ -98,41 +41,38 @@ var Menu = function () {
             },
 
             invalidHandler: function (event, validator) { //display error alert on form submit
-                $('.alert-error', $('.login-form')).show();
+                $('.alert-error', $('.menu-form')).show();
             },
 
             highlight: function (element) { // hightlight error inputs
-                $(element)
-                    .closest('.control-group').addClass('error'); // set error class to the control group
+                $(element).closest('.control-group').addClass('error'); // set error class to the control group
             },
 
             success: function (label) {
-
                 label.closest('.control-group').removeClass('error');
                 label.remove();
             },
 
             errorPlacement: function (error, element) {
-                console.log(element);
                 error.addClass('help-small no-left-padding').insertAfter(element.closest('.validation'));
             }
         });
     };
     var manageMenuSortOrder = function () {
         $('#menu-list').sortable({
-            axis: 'y',
+            axis  : 'y',
             cursor: 'move',
             update: function (event, ui) {
                 var data = $(this).sortable('serialize');
                 $.ajax({
                     data: data,
                     type: 'POST',
-                    url: updateMenuSortOrder,
+                    url : updateMenuSortOrder,
                 });
             }
         });
-    }
-    var manageDropdown = function () {
+    };
+    var manageDropdown      = function () {
         $("#post-dropdown").toggleClass("hidden", $("#menu-type").val() != 1);
         $("#page-dropdown").toggleClass("hidden", $("#menu-type").val() != 2);
         $("#external-url-textbox").toggleClass("hidden", $("#menu-type").val() != 3);
@@ -141,24 +81,22 @@ var Menu = function () {
             $("#page-dropdown").toggleClass("hidden", $(this).val() != 2);
             $("#external-url-textbox").toggleClass("hidden", $(this).val() != 3);
         });
-    }
+    };
 
     return {
-
         //function to initiate User Listing Page
         initList: function () {
             App.init();
             initMenuDataTable();
-            deleteMenu();
+            App.initBulkDelete({'deleteElement': $('#deleteMenus'), 'deleteUrl': menuBulkDeleteUrl, 'deleteSuccessMsg': 'Menus deleted successfully.', 'dataTable': dataTable});
             manageMenuSortOrder();
         },
 
         //function to initiate User Add/Edit Page
         initManagement: function () {
-            App.setPage("table_managed");
             App.init();
             validateMenu();
             manageDropdown()
-        },
+        }
     };
 }();
--- public/theme/admin/assets/js/page.js
+++ public/theme/admin/assets/js/page.js
@@ -1,19 +1,19 @@
 var Page = function () {
     var dataTable;
-    var initPageDataTable = function () {
+    var initPageDataTable  = function () {
         dataTable = $('#page-grid').DataTable({
             "processing": true,
             "serverSide": true,
-            "order": [[1, "asc"]],
+            "order"     : [[1, "asc"]],
             "columnDefs": [{
-                "targets": 0,
-                "orderable": false,
+                "targets"   : 0,
+                "orderable" : false,
                 "searchable": false
 
             }],
-            "ajax": {
-                url: pageAjaxPaginateUrl, // json datasource
-                type: "post",  // method  , by default get
+            "ajax"      : {
+                url  : pageAjaxPaginateUrl, // json datasource
+                type : "post",  // method  , by default get
                 error: function (data) {  // error handling
                     $(".user-grid-error").html("");
                     $("#user-grid").append('<tbody class="user-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
@@ -23,76 +23,19 @@ var Page = function () {
             }
         });
     };
-
-    var deletePage = function () {
-        $("#bulkDelete").on('click', function () { // bulk checked
-            var status = this.checked;
-            $(".deleteRow").each(function () {
-                $(this).prop("checked", status);
-            });
-            if ($("#bulkDelete").is(':checked')) {
-                $("#delete-btn").removeClass('hidden');
-            } else {
-                $("#delete-btn").addClass('hidden')
-            }
-        });
-        $(document).on('click', '.deleteRow', function (event) {
-            $("#delete-btn").addClass('hidden')
-            var $checked = false;
-
-            $.each($(".deleteRow"), function (index, value) {
-                if ($(this).is(':checked')) {
-                    $checked = true;
-                    $("#delete-btn").addClass('hidden')
-                }
-            });
-
-            if ($checked) {
-                $("#delete-btn").removeClass('hidden');
-            } else {
-                $('#bulkDelete').prop("checked", false);
-            }
-
-        });
-
-        $('#deletePages').on("click", function (event) { // triggering delete one by one
-            if ($('.deleteRow:checked').length > 0) {  // at-least one checkbox checked
-                var ids = [];
-                $('.deleteRow').each(function () {
-                    if ($(this).is(':checked')) {
-                        ids.push($(this).val());
-                    }
-                });
-                var ids_string = ids.toString();
-                // array to string conversion
-                $.ajax({
-                    type: "POST",
-                    url: pageBulkDeleteUrl,
-                    data: {data_ids: ids_string},
-                    success: function (result) {
-                        dataTable.draw(); // redrawing datatable
-                        $('.header-title').after('<div class="alert alert-success">' +
-                            '<strong>' + 'Pages deleted successfully.' + '</strong></div>');
-                    },
-                    async: false
-                });
-            }
-        });
-    };
-
-    var validatePage = function () {
+    var validatePage       = function () {
         $('.page-form').validate({
             errorElement: 'label', //default input error message container
-            errorClass: 'help-inline', // default input error message class
+            errorClass  : 'help-inline', // default input error message class
             focusInvalid: false, // do not focus the last invalid input
-            rules: {
-                name: {
+            rules       : {
+                name          : {
                     required: true
                 },
-                views:{
+                views         : {
                     number: true
                 },
-                featured_image:{
+                featured_image: {
                     accept: "image/*"
                 }
 
@@ -100,19 +43,19 @@ var Page = function () {
 
 
             messages: {
-                name: {
+                name          : {
                     required: "Page name is required."
                 },
-                views:{
+                views         : {
                     number: "Please enter valid number."
                 },
-                featured_image:{
+                featured_image: {
                     accept: 'Please upload Image file only.'
                 }
             },
 
             invalidHandler: function (event, validator) { //display error alert on form submit
-                $('.alert-error', $('.login-form')).show();
+                $('.alert-error', $('.page-form')).show();
             },
 
             highlight: function (element) { // hightlight error inputs
@@ -121,7 +64,6 @@ var Page = function () {
             },
 
             success: function (label) {
-
                 label.closest('.control-group').removeClass('error');
                 label.remove();
             },
@@ -130,39 +72,39 @@ var Page = function () {
                 if (element.attr("name") == "featured_image") {
                     error.insertAfter(element.closest('.file-upload-button-area'));
                 } else {
-                    error.addClass('help-small no-left-padding').insertAfter(element.closest('.validation'));}}
+                    error.addClass('help-small no-left-padding').insertAfter(element.closest('.validation'));
+                }
+            }
         });
     };
-    var manageDeleteImage = function(){
+    var manageDeleteImage  = function () {
         $("#deleteImage").on('click', function () {
             $("input[name='delete_featured_image']").val(1);
             $('.featured-image-area').find('img').attr('src', 'http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image');
             $(".featured-image-delete-btn").hide();
         });
-    }
+    };
     var manageStatusButton = function () {
-        $("#status_submit").html('Save as '+$("#status-type").val());
+        $("#status_submit").html('Save as ' + $("#status-type").val());
         $("#status-type").on('click', function () {
-            $("#status_submit").html('Save as '+$("#status-type").val());
+            $("#status_submit").html('Save as ' + $("#status-type").val());
         });
-    }
+    };
 
     return {
-
         //function to initiate User Listing Page
         initList: function () {
             App.init();
             initPageDataTable();
-            deletePage();
+            App.initBulkDelete({'deleteElement': $('#deletePages'), 'deleteUrl': pageBulkDeleteUrl, 'deleteSuccessMsg': 'Pages deleted successfully.', 'dataTable': dataTable});
         },
 
         //function to initiate User Add/Edit Page
         initManagement: function () {
-            App.setPage("table_managed");
             App.init();
             validatePage();
             manageStatusButton();
             manageDeleteImage();
-        },
+        }
     };
 }();
