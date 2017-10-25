105c57b3ce6eeb0c50f47965174cbfb1c3a5924f
mehul <mehul.dobariya401@gmail.com>
2017-10-25 13:15:02 +0530

    Contact us page and Post page Changes [WR-133] [WR-132]

--- App/Views/Front/contactus.php
+++ App/Views/Front/contactus.php
@@ -7,13 +7,14 @@ $recaptcha = new Recaptcha();
 <div class="row">
     <div class="col-lg-12 bg-white p-4 details-block">
         <?php include(\App\Config::F_VIEW . 'Admin/notifications.php') ?>
-        <div class="details-content pb-4">
-            <p>Get in touch and we'll get back to you as soon as we can. We look forward to hearing from you!</p>
-
-            <h1>Contact Form</h1>
-        </div>
         <div class="row">
-            <div class="col-xl-6">
+            <div class="col-lg-6 m-auto">
+                <div class="details-content text-center m-auto content-form-info">
+                    <p>Get in touch and we'll get back to you as soon as we can. We look forward to hearing from you!</p>
+                    <p>Email: <a class="text-black" href="mailto:therealwallreport@gmail.com">therealwallreport@gmail.com</a></p>
+                    <h1>Contact Form</h1>
+                </div>
+
                 <form method="post" class="contact-us-form" action="<?php echo \App\Config::W_ROOT . "contact-us" ?>">
                     <input type="hidden" name="token" value="<?php echo \Core\Csrf::getToken(); ?>">
 
@@ -43,15 +44,6 @@ $recaptcha = new Recaptcha();
                         <input type="submit" name="submit" class="btn btn-black" id="button" value="Send">
                     </div>
                 </form>
-            </div>
-            <div class="col-xl-6">
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Email</label>
-
-                    <div><a class="text-black" href="mailto:therealwallreport@gmail.com">therealwallreport@gmail.com</a>
-                    </div>
-                </div>
-
             </div>
         </div>
     </div>
