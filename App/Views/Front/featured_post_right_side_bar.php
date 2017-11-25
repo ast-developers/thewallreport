6edf456b087fe04f19572ad91df5c83f89107460
preetibendal <preeti.bendal@arsenaltech.com>
2017-11-25 14:37:32 +0530

    Show post views number on meta details. Format the numbers. Provide links in featured link sliders.

--- App/Views/Front/featured_post_right_side_bar.php
+++ App/Views/Front/featured_post_right_side_bar.php
@@ -7,9 +7,9 @@
                 $image = \Core\Helper::getCMSFeaturedImage($banner, '360x240');
                 ?>
                 <div class="item">
-                    <figure><a href="#"><img src="<?php echo $image; ?>"></a></figure>
+                    <figure><a href="<?php echo \App\Config::W_ROOT . $banner['slug'] ?>"><img src="<?php echo $image; ?>"></a></figure>
                     <h5>
-                        <a href="#"><?php echo $banner['name'] ?></a>
+                        <a href="<?php echo \App\Config::W_ROOT . $banner['slug'] ?>"><?php echo $banner['name'] ?></a>
                     </h5>
                     <span class="cb-date">
                         <time class="updated" datetime="2017-06-30"><?php echo date("F j, Y", strtotime($banner['published_at'])) ?></time>
