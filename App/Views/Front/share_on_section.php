a25f60a872391444e086660d8ab2af2bfb6e4a83
preetibendal <preeti.bendal@arsenaltech.com>
2017-12-07 15:03:57 +0530

    WR-74 - Fb Sharer on Detail Pages (#58)
    * WR-68 - Resolved issue of links for Twitter feeds
    
    * WR-68 - Resolved issue of tags in twitter feeds not getting searched and opening on popup
    
    * WR-63 - Stream bottom part issue for feed selection box
    
    * WR-65 - Implemented Thumb Image generation in Featured Images in Post and Page
    
    * WR-73 - Feed cards loading
    
    * WR-64 - Overlapping items over the ads
    
    * WR-74 - Fb Sharer on Detail Pages

--- App/Views/Front/share_on_section.php
+++ App/Views/Front/share_on_section.php
@@ -4,8 +4,15 @@
             <h4>SHARE ON</h4>
         </div>
         <div class="col-md-8 d-flex align-items-center justify-content-md-end">
-            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/"
-                 data-layout="button_count"></div>
+            <!--<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/"
+                 data-layout="button_count"></div>-->
+            <?php
+            $fbUrl = \App\Config::W_ROOT.$slug;
+            if(isset($feed)){
+                $fbUrl = \App\Config::W_ROOT.'feed/'.$feed['post_id'].'/'.$feed['feed_id'];
+            }
+            ?>
+            <div class="fb-share-button" data-href="<?php echo $fbUrl; ?>" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $fbUrl; ?>&amp;src=sdkpreparse"></a></div>
 
             <iframe
                 src="https://platform.twitter.com/widgets/tweet_button.html?size=l&url=<?php echo \App\Config::W_ROOT . $slug; ?>&related=twitterapi%2Ctwitter&text=<?php echo $header; ?> The Wall Report"
